23ba1d823129f2ff70bb3347df8731476e172b80
Ankit Patel <60725400+Pate0590@users.noreply.github.com>
2023-10-09 18:52:58 -0400

    crud

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 # post_service.py
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 
 app = Flask(__name__)
+posts = {
+        '1': {'user_id': '1', 'post': 'Hello, world!'},
+        '2': {'user_id': '2', 'post': 'My first blog post'}
+    }
 
 @app.route('/')
 def hello():
@@ -11,19 +15,58 @@ def hello():
 
 @app.route('/post/<id>')
 def post(id):
-    posts = {
-        '1': {'user_id': '1', 'post': 'Hello, world!'},
-        '2': {'user_id': '2', 'post': 'My first blog post'}
-    }
+    
     post_info = posts.get(id, {})
     
     # Get user info from User Service
     if post_info:
-        response = requests.get(f'http://ankituserservice.azurewebsites.net/user/{post_info["user_id"]}')
+        response = requests.get(f'http://localhost:5001/user/{post_info["user_id"]}')
         if response.status_code == 200:
             post_info['user'] = response.json()
 
     return jsonify(post_info)
+@app.route('/post', methods=['POST'])
+def create_post():
+    new_post = request.get_json()
+    
+    # Define a list of required keys
+    required_keys = ['user_id',  'post']
+
+    # Check if all required keys exist in the request data
+    if all(key in new_post for key in required_keys):
+        posts[str(len(posts.keys()) + 1)] = new_post
+        print(posts)
+        return jsonify({"success":True})
+    else:
+        return jsonify({"success":False, "msg": "Please pass all the data"})
+    
+@app.route('/post/<id>', methods=['PUT'])
+def update_post(id):
+    # Check if the post ID exists
+    if id in posts:
+        updated_post = request.get_json()
+
+        # Check if the required keys are present in the request data
+        required_keys = ['user_id',  'post']
+        if all(key in updated_post for key in required_keys):
+            posts[id] = updated_post
+            print(posts)
+            return jsonify({"success": True, "msg": "post updated successfully"})
+        else:
+            return jsonify({"success": False, "msg": "Please pass all the required data for update"}), 400
+    else:
+        return jsonify({"success": False, "msg": "post not found"}), 404
+    
+@app.route('/post/<id>', methods=['DELETE'])
+def delete_post(id):
+    if id in posts:
+        # If the post with the given ID exists, delete it
+        del posts[id]
+        return jsonify({"success": True, "msg": "post deleted successfully"})
+    else:
+        return jsonify({"success": False, "msg": "post not found"}), 404
+
+
 
 if __name__ == '__main__':
     app.run(port=5001)
